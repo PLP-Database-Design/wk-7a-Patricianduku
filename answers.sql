a8e431fb0419a421140b24cf4dab5088f7c7a6d1
Patricianduku <ndukupatricia136@gmail.com>
2025-05-05 10:51:14 +0300

    updated answers.sql

--- answers.sql
+++ answers.sql
@@ -1,38 +1,47 @@
 --  Question 1
-CREATE TABLE ProductDetail_1NF (
+CREATE TABLE ProductDetail (
     OrderID INT,
     CustomerName VARCHAR(100),
-    Product VARCHAR(100),
-    PRIMARY KEY (OrderID, Product)
+    Products VARCHAR(100),
+PRIMARY KEY (OrderID, Products)
 );
-INSERT INTO ProductDetail_1NF (OrderID, CustomerName, Product)
-VALUES 
-    (101, 'John Doe', 'Laptop'),
-    (101, 'John Doe', 'Mouse'),
-    (102, 'Jane Smith', 'Tablet'),
-    (102, 'Jane Smith', 'Keyboard'),
-    (102, 'Jane Smith', 'Mouse'),
-    (103, 'Emily Clark', 'Phone');
+INSERT INTO ProductDetail(OrderID, CustomerName, Products)
+VALUES
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
+
 
     --  Question 2
-    
     CREATE TABLE Orders (
     OrderID INT PRIMARY KEY,
     CustomerName VARCHAR(100)
 );
-
 INSERT INTO Orders (OrderID, CustomerName)
-SELECT DISTINCT OrderID, CustomerName
-FROM OrderDetails;
+VALUES
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
 
-CREATE TABLE OrderDetails_2NF (
+
+CREATE TABLE Product (
     OrderID INT,
     Product VARCHAR(100),
     Quantity INT,
     PRIMARY KEY (OrderID, Product),
     FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
 );
+INSERT INTO Product (OrderID, Product, Quantity)
+VALUES
+(101, 'Laptop', 2),
+(101, 'Mouse', 1),
+(102, 'Tablet', 3),
+(102, 'Keyboard', 1),
+(102, 'Mouse', 2),
+(103, 'Phone', 1);
 
-INSERT INTO OrderDetails_2NF (OrderID, Product, Quantity)
-SELECT OrderID, Product, Quantity
-FROM OrderDetails;
+
+   
